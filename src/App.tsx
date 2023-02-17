ab30cf1fc17a1761e56767fd59ff9220aef51172
Iuriiiiii <alex.casas@webtroniclabs.com>
2023-02-17 13:29:23 -0300

    +logic update

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ import mainVideo from '/santa-main.mp4';
 import bodyCare from '/santa-cuidado-del-cuerpo.mp4';
 import lack from '/santa-escasez.mp4';
 import noneVideo from '/santa-none.mp4';
+import missVideo from '/santa-extrañas-a-alguien.mp4';
+import { debug, getOpenaiAnswer } from './utils';
 
 const videosDatabase = {
   'Cuidado personal': bodyCare,
   'None': noneVideo,
   'Escasez': lack,
-  'Futuro': lack
+  'Futuro': lack,
+  'Extrañar a alguien': missVideo
 };
 
 function App() {
   const inputTextRef = useRef<HTMLInputElement>(null);
   const chatContainer = useRef<HTMLDivElement>(null);
-  const videoContainer = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
   const [messages, setMessages] = useState<string[]>([]);
   /* filo stack */
   const [videos, setVideos] = useState<string[]>([]);
@@ -29,72 +32,14 @@ function App() {
   }, [messages.length]);
 
   useEffect(() => {
-    if (videoContainer.current) {
-      videoContainer.current.play();
+    if (videoRef.current) {
+      videoRef.current.play();
     }
 
-    console.log('Video: ', videos.at(0) || mainVideo);
+    if (debug()) {
+      console.log('Video: ', videos.at(0) || mainVideo);
+    }
   }, [videos.length]);
-
-  async function getOpenaiAnswer(text: string) {
-    const configuration = new Configuration({
-      apiKey: import.meta.env.VITE_OPENAI_SECRET_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-    // const header = 'Answer in one word or "None". Which of the following strings "Love", "Friendship", "Feelings", "Work", "Money" describes better the following text?:';
-    const header = 'Textos: "Cuidado del cuerpo", "Futuro" y "Escasez".\nResponde única y estrictamente con el texto que mejor describa la siguiente sentencia o con "None": ';
-    const prompt = header + text.trim();
-
-    console.log({ prompt });
-    // ¿Qué puedo esperar del futuro?
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt,
-      temperature: 0.7,
-      max_tokens: 50,
-      n: 1,
-      stop: '\n'
-    });
-
-    console.log(completion.data.choices);
-
-    return completion.data.choices[0].text;
-  }
-
-  async function getSynthesiaVideo(text: string, notify: (response: SynthesiaVideoResponse) => void) {
-    const synth = new Synthesia({
-      token: import.meta.env.VITE_SYNTHESIA_TOKEN,
-      test: import.meta.env.VITE_SYNTHESIA_TEST_MODE
-    });
-
-    console.time('Synthesia create video request');
-    console.log('createVideo');
-    const createVideo = await synth.request(new SynthesiaCreateVideo([
-      new Input({
-        scriptText: text,
-        avatar: import.meta.env.VITE_SYNTHESIA_AVATAR,
-      })
-    ]));
-    console.timeEnd('Synthesia create video request');
-    console.log('end createVideo');
-
-    if (createVideo.status === 201) {
-      const interval = setInterval(async () => {
-        console.time('Synthesia get video request');
-        const getVideo = await synth.request(new SynthesiaGetVideo(createVideo.data.id));
-        console.timeEnd('Synthesia get video request');
-
-
-        console.log({ status: getVideo.data.status });
-
-        if (getVideo.status > 205 || getVideo.data.status === 'complete') {
-          notify(getVideo.data);
-          return clearInterval(interval);
-        }
-      }, 10000);
-    }
-
-  }
 
   async function clickHandler(e: React.MouseEvent<HTMLButtonElement>) {
     if (!inputTextRef) {
@@ -107,35 +52,40 @@ function App() {
       return;
     }
 
-    console.log('Question:', question);
-    console.time('OpenAI');
-    const chatGPTAnswer = (await getOpenaiAnswer(question))!.split(/\s/g).at(-1);
-    console.timeEnd('OpenAI');
-    // console.log('End question');
+    if (debug()) {
+      console.time('OpenAI');
+    }
+
+    const chatGPTAnswer = ((await getOpenaiAnswer(question))!.split('\n\n')[1] || '').trim().replace('"', '');
+
+    if (debug()) {
+      console.timeEnd('OpenAI');
+    }
+
     inputTextRef.current!.value = '';
-    // setMessages([...messages, chatGPTAnswer]);
-    // chatContainer.current!.scrollTop = chatContainer.current!.scrollHeight;
 
-    // getSynthesiaVideo(chatGPTAnswer, (response) => {
-    //   setMessages([...messages, chatGPTAnswer]);
-    //   pushVideo(response.download!);
-    //   // console.log(response.download);
-    //   console.timeEnd("Synthesia");
-
-    // });
-
-    console.log('Item:', chatGPTAnswer);
-
+    if (debug()) {
+      console.log('OpenAI text:', chatGPTAnswer);
+    }
     /* @ts-ignore */
-    pushVideo(videosDatabase[chatGPTAnswer.replace(/[^0-9a-z]/gi, '')]);
+    setVideos([...videos, videosDatabase[chatGPTAnswer]]);
   }
 
-  function pushVideo(url: string) {
-    console.log(url);
-    setVideos([...videos, url]);
+  function getMedia() {
+    return videos[0] || mainVideo;
   }
 
-  function nextVideo() {
+  function onVideoClick(e: React.MouseEvent<HTMLVideoElement>) {
+    const current = e.currentTarget;
+
+    if (current.paused) {
+      return current.play();
+    }
+
+    current.pause();
+  }
+
+  function getNextVideo() {
     if (videos.length === 0) {
       return;
     }
@@ -144,19 +94,9 @@ function App() {
     setVideos([...videos]);
   }
 
-  function playVideo() {
-    if (videoContainer.current) {
-      videoContainer.current.play();
-    }
-  }
-
-  function onVideoEnd() {
-    nextVideo();
-  }
-
   return (
     <div className='flex justify-center items-center w-screen h-screen bg-[#E1B6B6]'>
-      <Player cref={videoContainer} onClick={playVideo} className='w-full h-screen' src={videos.at(0) || mainVideo} onEnded={onVideoEnd} autoPlay />
+      <Player cref={videoRef} onClick={onVideoClick} className='w-full h-screen' src={getMedia()} onEnded={getNextVideo} autoPlay />
       <div className='flex flex-col space-y-2 w-4/6 absolute bottom-2 drop-shadow-2xl opacity-80'>
         <div ref={chatContainer} className='flex flex-col space-y-2 h-32 w-full text-white bg-pink-800 overflow-y-scroll p-3'>
           {messages.map((message, index) => <span key={`${message}${index}`}>{message}</span>)}
---
+++ src/utils/index.ts
@@ -0,0 +1,78 @@
+import { Configuration, OpenAIApi } from 'openai';
+import { Input, Synthesia, SynthesiaCreateVideo, SynthesiaGetVideo, SynthesiaVideoResponse } from '../../lib/synthesia';
+
+export function debug(): boolean {
+    return import.meta.env.VITE_DEBUG;
+}
+
+export async function getSynthesiaVideo(text: string, notify: (response: SynthesiaVideoResponse) => void) {
+    const synth = new Synthesia({
+        token: import.meta.env.VITE_SYNTHESIA_TOKEN,
+        test: import.meta.env.VITE_SYNTHESIA_TEST_MODE
+    });
+
+    if (debug()) {
+        console.time('Synthesia create video request');
+    }
+
+    const createVideo = await synth.request(new SynthesiaCreateVideo([
+        new Input({
+            scriptText: text,
+            avatar: import.meta.env.VITE_SYNTHESIA_AVATAR,
+        })
+    ]));
+
+    if (debug()) {
+        console.timeEnd('Synthesia create video request');
+    }
+
+    if (createVideo.status === 201) {
+        let debugCounter = 0;
+
+        const interval = setInterval(async () => {
+            if (debug()) {
+                console.time('Synthesia get video request Nº' + debugCounter);
+            }
+
+            const getVideo = await synth.request(new SynthesiaGetVideo(createVideo.data.id));
+
+            if (debug()) {
+                console.timeEnd('Synthesia get video request Nº' + debugCounter);
+                debugCounter++;
+            }
+
+            if (getVideo.status > 205 || getVideo.data.status === 'complete') {
+                notify(getVideo.data);
+                return clearInterval(interval);
+            }
+        }, 10000);
+    }
+
+}
+
+export async function getOpenaiAnswer(text: string) {
+    const configuration = new Configuration({
+        apiKey: import.meta.env.VITE_OPENAI_SECRET_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    // const header = 'Answer in one word or "None". Which of the following strings "Love", "Friendship", "Feelings", "Work", "Money" describes better the following text?:';
+    const header = `
+Textos: "Cuidado del cuerpo", "Esperar del futuro", "Futuro", "Extrañar a alguien" y "Escasez".
+Responde única y estrictamente con el texto que mejor describa la siguiente sentencia, si nunguna de ellas encaja, response con "None":
+`.trim();
+    const prompt = header + ' ' + text.trim();
+
+    console.log({ prompt });
+    // ¿Qué puedo esperar del futuro?
+    const completion = await openai.createCompletion({
+        model: 'text-davinci-002',
+        prompt,
+        temperature: 0.6,
+        max_tokens: 50,
+        n: 1,
+    });
+
+    console.log(completion.data.choices);
+
+    return completion.data.choices[0].text;
+}
